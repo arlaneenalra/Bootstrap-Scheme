564b28cc8eaa262b32fc787e24cbee882f040dc7
Chris Salch <emeraldd.chris@gmail.com>
2010-01-06 21:12:03 -0600

    Starting to work out some basic memory allocation

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
+
+#include "core.h"
 
 int main(int argc, char **argv) {
-	printf("Simple Bootstrapper\n");
-	printf(">");
-	exit(0);
+
+    printf("Simple Bootstrapper\n");
+    printf("sizeof(object_type) %li\n", sizeof(object_type));
+
+    printf(">\n");
+
+    exit(0);
 }
---
+++ src/core.h
@@ -0,0 +1,37 @@
+#ifndef _CORE_
+#define _CORE_
+
+#include <stdio.h>
+#include <wchar.h>
+
+#include <stdint.h>
+
+/* The types of cells that we can represent */
+typedef enum {
+    INTEGER,
+    BOOL,
+    CHAR
+} object_type_enum;
+
+/* Define a structure to represent a memory cell */
+typedef struct object {
+    object_type_enum type;
+
+    union {
+	int64_t int_val;
+	wchar_t char_val; 
+    } value;
+
+} object_type;
+
+
+
+
+/* Values that are required for an instance of the interpreter 
+   to function properly */
+typedef struct interp_core {
+    
+} interp_core_type;
+
+
+#endif
