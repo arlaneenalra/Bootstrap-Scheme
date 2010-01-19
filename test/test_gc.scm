a8c446ddbd596114404e06fdd8a0d5fa8e658d44
Chris Salch <emeraldd.chris@gmail.com>
2010-01-19 01:23:18 -0600

    A short script to exercise the gc

---
+++ test/test_gc.scm
@@ -0,0 +1,22 @@
+; allocate a large number of objects
+
+(define numbers '())
+;(cons (number->string 1) numbers)
+;(quit)
+(define massalloc
+  (lambda (num)
+    (+ 1 1 )
+    (set! numbers (cons (number->string num) numbers))
+    (if (= num 0)
+	numbers
+	(massalloc (- num 1)))))
+
+(massalloc 10)
+
+(gc_stats)
+
+(gc_stats 1)
+
+(gc_stats)
+
+(quit)
