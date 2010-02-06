9681e2e37ecc244c40256bcdcb2b00d40578d9f8
Chris Salch <emeraldd.chris@gmail.com>
2010-02-06 00:47:03 -0600

    Load the string functions

---
+++ lib/stdlib.scm
@@ -0,0 +1,11 @@
+; Define some rather basic operations in scheme
+
+;; ; define cons in scheme
+;; (define (cons x y)
+;;   (define pair '( () . () ))
+;;   (set-car! pair x)
+;;   (set-cdr! pair y)
+;;   pair)
+
+; load the string code library
+(load "lib/string.h")
