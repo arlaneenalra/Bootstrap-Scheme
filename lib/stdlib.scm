cb909a77e5ed4918d3a383dcad258aeef3304cfd
Chris Salch <emeraldd.chris@gmail.com>
2010-02-06 01:09:31 -0600

    Some useful utilities

--- lib/stdlib.scm
+++ lib/stdlib.scm
@@ -8,4 +8,37 @@
 ;;   pair)
 
 ; load the string code library
+
 (load "lib/string.scm")
+
+
+
+; Display a new line
+(define (newline)
+  (write-char #\newline))
+
+; output a string to the current default output
+; port
+(define (emit-string str) 
+  (define (inner index len)
+    (if (< index len)
+	(begin
+	  (write-char (string-ref str index))
+	  (inner (+ 1 index) len))
+	))
+  (inner 0 (string-length str)))
+
+
+(define (emit-fixnum num)
+  (emit-string
+   (number->string num)))
+
+
+; TODO: find the real definition of emit
+(define (emit obj)
+  (cond
+   ((string? obj) (emit-string obj))
+   ((integer? obj) (emit-fixnum obj))
+   ((char? obj) (write-char obj))
+   (else (error "I don't know what this is"))
+   ))
