0278553fd58f561056f8d1c2ae68ce8cca924782
Chris Salch <emeraldd.chris@gmail.com>
2010-02-07 21:33:32 -0600

    filtered out some cmake files

--- count.sh
+++ count.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v test`
+wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\|CMake'`
