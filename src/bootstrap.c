8b4c2f8de530da5c6729e92dcbfe4b2f34d11a82
Chris Salch <emeraldd.chris@gmail.com>
2010-02-12 08:55:36 -0600

    Removed debugging code

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -15,9 +15,7 @@ int main(int argc, char **argv) {
     if(argc==1) {
 	repl(interp);
     } else {
-	printf("Loading %s\n",argv[1]);
 	top_level_program(interp, argv[1]);
-	printf("Done.\n");
     }
 
     return 0;
