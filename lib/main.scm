f942967f5c98fed573b10655a36035326112c061
Chris Salch <emeraldd.chris@gmail.com>
2010-02-11 23:01:02 -0600

    Hoping to get the repl into scheme and out of C as it will make macros easier to deal with

--- lib/main.scm
+++ lib/main.scm
@@ -2,3 +2,14 @@
 
 (load "lib/stdlib.scm")
 
+
+
+;; definintions for a simple repl
+;;(define env (interaction-environment))
+
+;; (define (repl)
+;;   (write-char #\])
+;;   (write (eval (read) env))
+;;   (write-char #\newline)
+;;   (repl))
+
