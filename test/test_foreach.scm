27a61522dbaf854c6bcb490ca8cf3ec9f8261fb4
Chris Salch <emeraldd.chris@gmail.com>
2010-02-12 08:50:16 -0600

    This belongs under test not in the root

--- test/test_foreach.scm
+++ test/test_foreach.scm
@@ -13,5 +13,10 @@
  '(1 2 3 4 5 6 7 8 9 10))
 
 
-(test (lambda () val-list) '(2 4 6 8 10 12 14 16 18 20))
+(if (test (lambda () val-list) '(2 4 6 8 10 12 14 16 18 20))
+    (write "GOOD")
+    (write "FAIL"))
+(write-char #\newline)
+
+
 
