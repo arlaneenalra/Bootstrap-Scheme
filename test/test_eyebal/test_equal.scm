9ddb5acc0878b66076da465ab9284a696acb441b
Chris Salch <emeraldd.chris@gmail.com>
2010-02-12 20:36:41 -0600

    equal? can walk a tree of values

--- test/test_eyebal/test_equal.scm
+++ test/test_eyebal/test_equal.scm
@@ -4,6 +4,11 @@
 (define b '(1 2 3 4))
 (define c '(1 2 3 ))
 
+(define abc (cons a (cons b (cons c '()))))
+(define ABC (cons a (cons b (cons c '()))))
+(define acb (cons a (cons c (cons b '()))))
+
+
 (newline)
 
 (equal? a a) ; #t
@@ -11,3 +16,17 @@
 (equal? b a) ; #t
 (equal? c a) ; #f 
 (equal? a c) ; #f
+
+(newline)
+
+(equal? abc abc) ; #t
+(equal? abc ABC) ; #t
+(equal? ABC abc) ; #t
+(equal? acb abc) ; #f 
+(equal? abc acb) ; #f
+
+(newline)
+
+(equal? abc a) ; #f
+
+
