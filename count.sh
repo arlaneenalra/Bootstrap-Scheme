fb411cb47786def7f84acb478fa703ac4c43a4d3
Chris Salch <emeraldd.chris@gmail.com>
2010-02-13 22:03:17 -0600

    Adjusted count.sh to count non-eyeball test suites

--- count.sh
+++ count.sh
@@ -4,7 +4,7 @@ make clean
 echo
 
 echo "Source Statistics:"
-wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\|CMake'`
+wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\/test_eyeball\|CMake'`
 
 echo
 echo "Commit Count: " `git log | grep '^commit' | wc -l`
