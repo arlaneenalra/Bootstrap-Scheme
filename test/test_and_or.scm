38b3f57c67967cc2fe71d5a27cd0809a576f277c
Chris Salch <emeraldd.chris@gmail.com>
2010-02-13 21:07:11 -0600

    Tests for and/or now match up with the r6rs spec

--- test/test_and_or.scm
+++ test/test_and_or.scm
@@ -12,10 +12,19 @@
    ((lambda () (and #t #f #t)) . #f)
    ((lambda () (and #t 2 #t)) . #t)
 
+   ((lambda () (and (= 2 2) (> 2 1))) . #t)
+   ((lambda () (and (= 2 2) (< 2 1))) . #f)
+   ((lambda () (and 1 2 'c '(f g))) . (f g))
+
    ((lambda () (or)) . #f)
    ((lambda () (or #f)) . #f)
    ((lambda () (or #t #t #t)) . #t)
    ((lambda () (or #t #f #t)) . #t)
-   ((lambda () (or #t 2 #t)) . #t)))
+   ((lambda () (or #t 2 #t)) . #t)
+   
+   ((lambda () (or (= 2 2) (> 2 1))) . #t)
+   ((lambda () (or (= 2 2) (< 2 1))) . #t)
+   ((lambda () (or #f #f #f)) . #f)
+   ((lambda () (or '(b c) (/ 3 0))) . (b c))))
 
 
