33fdf0daeedf84d12a97d0b1d24e2db1638033b0
Chris Salch <emeraldd.chris@gmail.com>
2010-02-13 22:00:00 -0600

    Now that I have the test data right, it would appear that the car/cdr tests are complete

--- test/test_car_cdr.scm
+++ test/test_car_cdr.scm
@@ -21,7 +21,6 @@
 
    ((lambda ()
       (cadr test-val)) . 2)
-
    ((lambda ()
       (cddr test-val)) . (3 4 5 6 7 8 9 10))
 
@@ -39,13 +38,51 @@
       (caadr '(#f . ((#t . #f) . #f)))) . #t)
    ((lambda ()
       (cdadr '(#f . ((#f . #t) . #f)))) . #t)
-
    ((lambda ()
       (caddr '(#f . (#f . (#t . #f))))) . #t)
    ((lambda ()
       (cdddr test-val)) . (4 5 6 7 8 9 10))
 
-))
+
+   ((lambda ()
+      (caaaar test-val-car)) . ((1 . 2) . 3))
+   ((lambda ()
+      (cdaaar test-val-car)) . 4)
+   ((lambda ()
+      (cadaar '(((#f . (#t . #f))  . #f) . #f))) . #t)
+   ((lambda ()
+      (cddaar '(((1 2 3) . #f) . #f))) . (3))
+
+   ((lambda ()
+      (caadar '((#f . ((#t . #f) . #f)) . #f))) . #t)
+   ((lambda ()
+      (cdadar '((#f . ((#f . #t) . #f)) . #f))) . #t)
+   ((lambda ()
+      (caddar '((#f . (#f . (#t . #f))) . #f))) . #t)
+   ((lambda ()
+      (cdddar '((1 2 3 4 5) . #f))) . (4 5))
+
+   ((lambda ()
+      (caaadr '(#f . (((#t . #f) . #f) . #f)))) . #t)
+   ((lambda ()
+      (cdaadr '(#f . (((#f . #t) . #f) . #f)))) . #t)
+
+   ((lambda ()
+      (cadadr '(#f . ((#f . (#t . #f))  . #f)))) . #t)
+
+   ((lambda ()
+      (cddadr '(#f . ((1 2 3) . #f)))) . (3))
+
+   ((lambda ()
+      (caaddr '(#f . (#f . ((#t . #f) . #f))))) . #t)
+   ((lambda ()
+      (cdaddr '(#f #f . ((#f . #t) . #f)))) . #t)
+
+   ((lambda ()
+      (cadddr '(#f #f #f . (#t . #f)))) . #t)
+
+   ((lambda ()
+      (cddddr test-val)) . (5 6 7 8 9 10))))
 
    
 
