07906ac17101ab142c94917c68cf5dbe371e97a5
Chris Salch <emeraldd.chris@gmail.com>
2010-02-21 01:45:41 -0600

    Filling in the vector tests

--- test/test_vector.scm
+++ test/test_vector.scm
@@ -10,13 +10,27 @@
    ((lambda () (vector? '#())) . #t)
    ((lambda () (vector? "testing")) . #f)
 
+   ((lambda () (make-vector 3)) . #(() () ()))
+   ((lambda () (make-vector 0)) . #())
+   ((lambda () (make-vector 4 #\a)) . #(#\a #\a #\a #\a))
+
    ((lambda () (vector 4 #\b 'vector)) . #(4 #\b vector))
+   ((lambda () (vector 'a 'b 'c)) . #(a b c))
 
    ((lambda () (vector-length '#(1 2 3 4))) . 4)
    ((lambda () (vector-length '#())) . 0)
 
-   ((lambda () (make-vector 3)) . #(() () ()))
-   ((lambda () (make-vector 0)) . #())
-   ((lambda () (make-vector 4 #\a)) . #(#\a #\a #\a #\a))
+   ((lambda () (vector-ref '#(1 1 2 3 5 8 13 21) 5)) . 8)
+
+   ((lambda ()
+      (let ((vec (vector 0 '(2 2 2 2) "Anna")))
+	(vector-set! vec 1 '("Sue" "Sue"))
+	vec)) . #(0 ("Sue" "Sue") "Anna"))
+
+   ((lambda ()
+      (vector->list '#(dah dah didah))) . (dah dah didah))
+
+   ((lambda ()
+      (list->vector '(dididit dah))) . #(dididit dah))
 
    ))
