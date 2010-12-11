ed54bb0f84e6de398e29b3a954f8272d64cda544
Chris Salch <emeraldd.chris@gmail.com>
2010-12-11 03:12:21 -0600

    Cleaning up for out of source build

--- count.sh
+++ count.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-echo "Cleanning . . ."
-make clean
-echo
 
 echo "Source Statistics:"
-wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\/test_eyeball\|CMake'`
+wc `find . -regextype posix-basic -iregex '.*\.\(scm\|h\|c\|y\|l\)' | grep -v 'test\/test_eyeball\|CMake\|build\|vendor'`
 
 echo
 echo "Commit Count: " `git log | grep '^commit' | wc -l`
