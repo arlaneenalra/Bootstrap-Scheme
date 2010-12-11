5bf2583897720c2aa5ff6e1bfe9eacb70f31d6a1
Chris Salch <emeraldd.chris@gmail.com>
2010-12-11 02:59:11 -0600

    Working on converting tests

--- test/test_vector.scm
+++ test/test_vector.scm
@@ -27,84 +27,83 @@
  '#(2 3 4 5 6 7 8 9 10 11)
  '#(1 2 3 4 5 6 7 8 9 10))
 
-(test
- '(((lambda () '#(1 2 3 4 5)) . #(1 2 3 4 5 ))
-   ((lambda () '#(0 (2 2 2 2) "Anna")) . #(0 (2 2 2 2) "Anna"))
+(test-suite
+ (make-test ('#(1 2 3 4 5)) #(1 2 3 4 5 ))
+ (make-test ('#(0 (2 2 2 2) "Anna")) #(0 (2 2 2 2) "Anna"))
+ 
+ (make-test ((vector? '#(1 2 3 4))) #t)
+ (make-test ((vector? '(1 2 3 4))) #f)
+ (make-test ((vector? '#())) #t)
+ (make-test ((vector? "testing")) #f)
 
-   ((lambda () (vector? '#(1 2 3 4))) . #t)
-   ((lambda () (vector? '(1 2 3 4))) . #f)
-   ((lambda () (vector? '#())) . #t)
-   ((lambda () (vector? "testing")) . #f)
+ (make-test ((make-vector 3)) #(() () ()))
+ (make-test ((make-vector 0)) #())
+ (make-test ((make-vector 4 #\a)) #(#\a #\a #\a #\a))
 
-   ((lambda () (make-vector 3)) . #(() () ()))
-   ((lambda () (make-vector 0)) . #())
-   ((lambda () (make-vector 4 #\a)) . #(#\a #\a #\a #\a))
+ (make-test ((vector 4 #\b 'vector)) #(4 #\b vector))
+ (make-test ((vector 'a 'b 'c)) #(a b c))
 
-   ((lambda () (vector 4 #\b 'vector)) . #(4 #\b vector))
-   ((lambda () (vector 'a 'b 'c)) . #(a b c))
+ (make-test ((vector-length '#(1 2 3 4))) 4)
+ (make-test ((vector-length '#())) 0)
 
-   ((lambda () (vector-length '#(1 2 3 4))) . 4)
-   ((lambda () (vector-length '#())) . 0)
+ (make-test ((vector-ref '#(1 1 2 3 5 8 13 21) 5)) 8)
 
-   ((lambda () (vector-ref '#(1 1 2 3 5 8 13 21) 5)) . 8)
+ (make-test (
+    (let ((vec (vector 0 '(2 2 2 2) "Anna")))
+      (vector-set! vec 1 '("Sue" "Sue"))
+      vec)) #(0 ("Sue" "Sue") "Anna"))
 
-   ((lambda ()
-      (let ((vec (vector 0 '(2 2 2 2) "Anna")))
-	(vector-set! vec 1 '("Sue" "Sue"))
-	vec)) . #(0 ("Sue" "Sue") "Anna"))
+ (make-test (
+    (vector->list '#(dah dah didah))) (dah dah didah))
 
-   ((lambda ()
-      (vector->list '#(dah dah didah))) . (dah dah didah))
+ (make-test (
+    (list->vector '(dididit dah))) #(dididit dah))
+ 
+ (make-test (
+    (let ((vec '#(1 2 3 4 5 )))
+      (vector-fill! vec #\b)
+      vec)) #(#\b #\b #\b #\b #\b))
 
-   ((lambda ()
-      (list->vector '(dididit dah))) . #(dididit dah))
-   
-   ((lambda ()
-      (let ((vec '#(1 2 3 4 5 )))
-	(vector-fill! vec #\b)
-	vec)) . #(#\b #\b #\b #\b #\b))
+ (make-test (
+    (let ((vec '#()))
+      (vector-fill! vec #\b)
+      vec)) #())
+ 
+ ;; for-each testing
+ (make-test (val-list) (20 18 16 14 12 10 8 6 4 2))
+ (make-test (val-list2) (21 19 17 15 13 11 9 7 5 3))
+ (make-test (val-list3) (31 28 25 22 19 16 13 10 7 4))
 
-   ((lambda ()
-      (let ((vec '#()))
-   	(vector-fill! vec #\b)
-   	vec)) . #())
-   
-   ;; for-each testing
-   ((lambda () val-list) . (20 18 16 14 12 10 8 6 4 2))
-   ((lambda () val-list2) . (21 19 17 15 13 11 9 7 5 3))
-   ((lambda () val-list3) . (31 28 25 22 19 16 13 10 7 4))
+ ;; map testing
+ (make-test ((vector-map 
+              (lambda (x)
+                (+ x x))
+              '#(1 2 3 4 5 6 7 8 9 10))) #(2 4 6 8 10 12 14 16 18 20))
 
-   ;; map testing
-   ((lambda () (vector-map 
-		(lambda (x)
-		  (+ x x))
-		 '#(1 2 3 4 5 6 7 8 9 10))) . #(2 4 6 8 10 12 14 16 18 20))
+ (make-test ((vector-map 
+              (lambda (x y)
+                (+ x y))
+              '#(1 2 3 4 5 6 7 8 9 10)
+              '#(2 3 4 5 6 7 8 9 10 11))) #(3 5 7 9 11 13 15 17 19 21))
 
-   ((lambda () (vector-map 
-		(lambda (x y)
-		  (+ x y))
-		 '#(1 2 3 4 5 6 7 8 9 10)
-		 '#(2 3 4 5 6 7 8 9 10 11))) . #(3 5 7 9 11 13 15 17 19 21))
+ (make-test ((vector-map 
+              (lambda (x y z)
+                (+ x y z))
+              '#(1 2 3 4 5 6 7 8 9 10)
+              '#(2 3 4 5 6 7 8 9 10 11)
+              '#(1 2 3 4 5 6 7 8 9 10))) #(4 7 10 13 16 19 22 25 28 31))
 
-   ((lambda () (vector-map 
-		(lambda (x y z)
-		  (+ x y z))
-		'#(1 2 3 4 5 6 7 8 9 10)
-		'#(2 3 4 5 6 7 8 9 10 11)
-		'#(1 2 3 4 5 6 7 8 9 10))) . #(4 7 10 13 16 19 22 25 28 31))
+ (make-test ((vector-map 
+              cadr
+              '#((a b) (d e) (g h)))) #(b e h))
 
-   ((lambda () (vector-map 
-		cadr
-		'#((a b) (d e) (g h)))) . #(b e h))
+ (make-test ((vector-map 
+              +
+              '#(1 2 3) '#(4 5 6))) #(5 7 9))
 
-   ((lambda () (vector-map 
-		+
-		'#(1 2 3) '#(4 5 6))) . #(5 7 9))
-
-   ((lambda () 
-      (let ((count 0))
-	(vector-map (lambda (ignored)
-	       (set! count (+ count 1))
-	       count)
-	     '#(a b)))) . #(1 2))
-   ))
+ (make-test (
+             (let ((count 0))
+               (vector-map (lambda (ignored)
+                             (set! count (+ count 1))
+                             count)
+                           '#(a b)))) #(1 2)))
