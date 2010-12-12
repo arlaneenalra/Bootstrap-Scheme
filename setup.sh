c88dec54fdf78ae426406371e99cb97a2069c2b5
Chris Salch <emeraldd.chris@gmail.com>
2010-12-12 12:54:11 -0600

    Rebuild the build tree

--- setup.sh
+++ setup.sh
@@ -3,12 +3,16 @@
 TARGET=build
 
 echo 'Setting up an out of source build tree:'
-if [ ! -d $TARGET ] ; then 
-	echo "	Creating Directory: $TARGET"
-	mkdir $TARGET
-	ln -s ../lib $TARGET/lib
-	ln -s ../test $TARGET/test
+if [ -d $TARGET ] ; then 
+	echo "	Cleaning out old build: $TARGET"
+	rm -r $TARGET
 fi
+
+echo "	Creating Directory: $TARGET"
+mkdir $TARGET
+
+ln -s ../lib $TARGET/lib
+ln -s ../test $TARGET/test
 
 cd $TARGET
 
